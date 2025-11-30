f8abae88fc6cb5f53cb6fb71b5f705f314885469
Mohamed-Ezzat1 <mohamedezzatshawky1@gmail.com>
2025-11-30 20:42:08 +0200

    3c

---
+++ frontend/pages/src/App.test.jsx
@@ -0,0 +1,9 @@
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import App from "./App.jsx";
+
+test("App renders without crashing", () => {
+  const { container } = render(<App />);
+ 
+  expect(container).toBeInTheDocument();
+});
